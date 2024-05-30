ba0e00ca419769d304d639a435c5234b5f8f6632
komal020304 <114329658+komal020304@users.noreply.github.com>
2024-05-30 21:32:59 +0530

    final commit

---
+++ src/dummydata.js
@@ -0,0 +1,408 @@
+export const homeAbout = [
+  {
+    id: 1,
+    cover: "https://img.icons8.com/dotty/80/000000/storytelling.png",
+    title: "Home Tutions",
+    desc: "We provide personalized training services for students from certified tutors right at your home.",
+  },
+  {
+    id: 1,
+    cover: "https://img.icons8.com/ios/80/000000/diploma.png",
+    title: "Personal Guidance",
+    desc: "We undertake services such as motivational result-driven classes and stress releasing sessions.",
+  },
+  {
+    id: 1,
+    cover: "https://img.icons8.com/ios/80/000000/athlete.png",
+    title: "Mentoring",
+    desc: "We have expert & experienced faculty members who provide online tutoring for the kids to learn online.",
+  },
+  {
+    id: 1,
+    cover: "https://img.icons8.com/dotty/80/000000/exam.png",
+    title: "Competitive Exams",
+    desc: "We have vastly experienced staff members who will ensure your success in various competetive exams.",
+  },
+];
+export const awrapper = [
+  {
+    cover:
+      "https://img.icons8.com/external-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto/80/ffffff/external-graduation-education-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto.png",
+    data: "3,000",
+    title: "SUCCESS STORIES",
+  },
+
+  {
+    cover: "https://img.icons8.com/ios/80/ffffff/athlete.png",
+    data: "320",
+    title: "TRUSTED TUTORS",
+  },
+  {
+    cover:
+      "https://img.icons8.com/external-outline-icons-maxicons/80/ffffff/external-calender-insurance-outline-outline-icons-maxicons.png",
+    data: "1,000",
+    title: "SCHEDULES",
+  },
+  {
+    cover: "https://img.icons8.com/ios/80/ffffff/macbook-idea--v3.png",
+    data: "587",
+    title: "COURSES",
+  },
+];
+export const coursesCard = [
+  {
+    id: 1,
+    cover: "../images/courses/c1.png",
+    coursesName: "Introducing to Software Engineering",
+    courTeacher: [
+      {
+        dcover: "./images/bg3.jpg",
+        name: "by John Smith",
+        totalTime: "50 lectures (190 hrs)",
+      },
+    ],
+    priceAll: "$100 All Course",
+    pricePer: "$15 per month",
+  },
+  {
+    id: 2,
+    cover: "../images/courses/c2.png",
+    coursesName: "Enhancing Adobe Photoshop CC 2020 Skills",
+    courTeacher: [
+      {
+        dcover: "./images/bg3.jpg",
+        name: "by Ram Gurung",
+        totalTime: "30 lectures (125 hrs)",
+      },
+    ],
+    priceAll: "$200 All Course",
+    pricePer: "$25 per month",
+  },
+  {
+    id: 3,
+    cover: "../images/courses/c3.png",
+    coursesName: "HTML, CSS, and Javascript for Web Developers",
+    courTeacher: [
+      {
+        dcover: "./images/bg3.jpg",
+        name: "by Saroj Nepal",
+        totalTime: "50 lectures (50 hrs)",
+      },
+    ],
+    priceAll: "$50 All Course",
+    pricePer: "$5 per month",
+  },
+  {
+    id: 4,
+    cover: "../images/courses/c4.png",
+    coursesName: "Introducing to Programming with WordPress",
+    courTeacher: [
+      {
+        dcover: "./images/bg3.jpg",
+        name: "by Prabin Thapa Mager",
+        totalTime: "20 lectures (20 hrs)",
+      },
+    ],
+    priceAll: "$30 All Course",
+    pricePer: "$3 per month",
+  },
+  {
+    id: 5,
+    cover: "../images/courses/c5.png",
+    coursesName: "Introducing to Programming with ReactJS",
+    courTeacher: [
+      {
+        dcover: "./images/bg3.jpg",
+        name: "by Sunil Bishowkaram",
+        totalTime: "100 lectures (150 hrs)",
+      },
+    ],
+    priceAll: "$300 All Course",
+    pricePer: "$30 per month",
+  },
+  {
+    id: 6,
+    cover: "../images/courses/c6.png",
+    coursesName: "Learn Frontend Programming Language",
+    courTeacher: [
+      {
+        dcover: "./images/p",
+        name: "by Sunil Bishowkaram",
+        totalTime: "200 lectures (300 hrs)",
+      },
+    ],
+    priceAll: "$500 All Course",
+    pricePer: "$80 per month",
+  },
+  {
+    id: 7,
+    cover: "../images/courses/c7.png",
+    coursesName: "Introducing to with HTML / CSS",
+    courTeacher: [
+      {
+        dcover: "./images/bg3.jpg",
+        name: "by Price",
+        totalTime: "20 lectures (50 hrs)",
+      },
+    ],
+    priceAll: "$20 All Course",
+    pricePer: "$2 per month",
+  },
+  {
+    id: 8,
+    cover: "../images/courses/c8.png",
+    coursesName: "Introducing to with JAVA",
+    courTeacher: [
+      {
+        dcover: "./images/bg3.jpg",
+        name: "by Petter",
+        totalTime: "80 lectures (200 hrs)",
+      },
+    ],
+    priceAll: "$200 All Course",
+    pricePer: "$40 per month",
+  },
+  {
+    id: 9,
+    cover: "../images/courses/c9.png",
+    coursesName: "Introducing to with HTML / CSS / JAVASCRIPT",
+    courTeacher: [
+      {
+        dcover: "./images/p",
+        name: "by Petter",
+        totalTime: "50 lectures (50 hrs)",
+      },
+    ],
+    priceAll: "$20 All Course",
+    pricePer: "$3 per month",
+  },
+];
+export const online = [
+  {
+    cover: "./images/courses/online/o1.png",
+    hoverCover: "./images/courses/online/o1.1.png",
+    courseName: "UI/UX Design Courses",
+    course: "25 Courses",
+  },
+  {
+    cover: "./images/courses/online/o2.png",
+    hoverCover: "./images/courses/online/o2.1.png",
+    courseName: "Art & Design",
+    course: "25 Courses",
+  },
+  {
+    cover: "./images/courses/online/o3.png",
+    hoverCover: "./images/courses/online/o3.1.png",
+    courseName: "Computer Science",
+    course: "10 Courses",
+  },
+  {
+    cover: "./images/courses/online/o4.png",
+    hoverCover: "./images/courses/online/o4.1.png",
+    courseName: "History & Archeologic",
+    course: "15 Courses",
+  },
+  {
+    cover: "./images/courses/online/o5.png",
+    hoverCover: "./images/courses/online/o5.1.png",
+    courseName: "Software Engineering",
+    course: "30 Courses",
+  },
+  {
+    cover: "./images/courses/online/o6.png",
+    hoverCover: "./images/courses/online/o6.1.png",
+    courseName: "Information Software",
+    course: "60 Courses",
+  },
+  {
+    cover: "./images/courses/online/o7.png",
+    hoverCover: "./images/courses/online/o7.1.png",
+    courseName: "Health & Fitness",
+    course: "10 Courses",
+  },
+  {
+    cover: "./images/courses/online/o8.png",
+    hoverCover: "./images/courses/online/o8.1.png",
+    courseName: "Marketing",
+    course: "30 Courses",
+  },
+  {
+    cover: "./images/courses/online/o9.png",
+    hoverCover: "./images/courses/online/o9.1.png",
+    courseName: "Graphic Design",
+    course: "80 Courses",
+  },
+  {
+    cover: "./images/courses/online/o10.png",
+    hoverCover: "./images/courses/online/o10.1.png",
+    courseName: "Music",
+    course: "120 Courses",
+  },
+  {
+    cover: "./images/courses/online/o11.png",
+    hoverCover: "./images/courses/online/o11.1.png",
+    courseName: "Business Administration",
+    course: "17 Courses",
+  },
+  {
+    cover: "./images/courses/online/o12.png",
+    hoverCover: "./images/courses/online/o12.1.png",
+    courseName: "Web Management",
+    course: "17 Courses",
+  },
+];
+export const team = [
+  {
+    cover: "./images/team/t1.jpg",
+    name: "Sourabh Bohare",
+    work: "Director",
+  },
+  {
+    cover: "./images/team/t2.jpg",
+    name: "Ambarish Abgul",
+    work: "Director",
+  },
+  {
+    cover: "./images/team/t3.webp",
+    name: "Pranav Kachare",
+    work: "Director",
+  },
+  {
+    cover: "./images/team/t4.jpg",
+    name: "Sagar Pokharkar",
+    work: " CHrO",
+  },
+  {
+    cover: "./images/team/t5.jpg",
+    name: "Kshitij Ingole",
+    work: "CFO",
+  },
+];
+export const price = [
+  {
+    name: "BASIC PLAN",
+    price: " ",
+    desc: "For those who appreciate simplicity, affordabilty and reliability the BASIC PLAN offers essential, user-friendly, reliable features and services. ",
+  },
+  {
+    name: "BEGINNER PLAN",
+    price: " ",
+    desc: "Perfect for the newcomers, the BEGINNER PLAN provides seamless introduction with easy-to-use, fundamental and friendly support.",
+  },
+  {
+    name: "PREMIUM PLAN",
+    price: " ",
+    desc: "For those who demand the best, the PREMIUM PLAN delivers top-tier, exclusive, cutting-edge, business features and unparalleled services. ",
+  },
+  {
+    name: "ULTIMATE PLAN",
+    price: " ",
+    desc: "For those who seek perfection, the ULTIMATE PLAN offers unparalleled, comprehensive, and premium features and services. ",
+  },
+];
+export const faq = [
+  {
+    title: "What is Scholar Pro Connect LLP?",
+    desc: "Scholar Pro Connect LLP offers comprehensive consultancy services for college admissions and pre-college support, utilizing a personalized approach and cutting-edge technology.",
+  },
+  {
+    title: "What admissions are offered under Admission Consultancy?",
+    desc: "Our services include guidance on course selection, application assistance, essay and SOP writing, interview preparation and Scholarship advice for both PG and UG admissions.",
+  },
+  {
+    title: "How does the personalized approach work?",
+    desc: "Our personalized approach involves one-on-one consultations to understand each student's unique strengths, interests, and goals. We then tailor our services to create a customized roadmap for their college admission journey.",
+  },
+  {
+    title: "Can you help with Scholarship applications?",
+    desc: " Yes, we provide specialized assistance for scholarship applications.We aim to maximize your chances of securing financial aid for your education. ",
+  },
+  {
+    title: "How do you implement personalized learning for students?",
+    desc: "We start with one-on-one consultations to assess each student's unique strengths, challenges, and goals. Based on this, we create a customized learning plan with tailored resources and strategies to ensure effective learning and growth.",
+  },
+  {
+    title:
+      "What makes Scholar Pro Connect LLP different from other consultancy services?",
+    desc: " Our distinct blend of personalized attention, expert consultants, and innovative technology sets us apart. We are committed to empowering each student's educational journey with tailored strategies and unwavering support. ",
+  },
+];
+export const blog = [
+  {
+    id: 1,
+    type: "admin",
+    date: "MAR 25, 2024",
+    com: "3 COMMENTS ",
+    title: " ",
+    desc: " ",
+    cover: "../images/blog/coding.jpg",
+  },
+  {
+    id: 2,
+    type: "admin",
+    date: "APR 09, 2024",
+    com: "5 COMMENTS ",
+    title: " ",
+    desc: " ",
+    cover: "../images/blog/graduation.jpg",
+  },
+  {
+    id: 3,
+    type: "user",
+    date: "APR 17, 2024",
+    com: "10 COMMENTS ",
+    title: " ",
+    desc: " ",
+    cover: "../images/blog/library.jpg",
+  },
+  {
+    id: 4,
+    type: "admin",
+    date: "APR 24, 2024",
+    com: "3 COMMENTS ",
+    title: " ",
+    desc: " ",
+    cover: "../images/blog/children.jpg",
+  },
+  {
+    id: 5,
+    type: "admin",
+    date: "MAY 09, 2024",
+    com: "3 COMMENTS ",
+    title: " ",
+    desc: " ",
+    cover: "../images/blog/office_desk.jpg",
+  },
+  {
+    id: 6,
+    type: "user",
+    date: "MAY 22, 2024",
+    com: "12 COMMENTS ",
+    title: " ",
+    desc: " ",
+    cover: "../images/blog/boys.jpg",
+  },
+];
+export const testimonal = [
+  {
+    id: 1,
+    name: "ROGER SCOTT",
+    post: "MARKETING MANAGER",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    cover: "./images/testo/t1.webp",
+  },
+  {
+    id: 2,
+    name: "ROGER SCOTT",
+    post: "MARKETING MANAGER",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    cover: "./images/testo/t2.webp",
+  },
+  {
+    id: 3,
+    name: "ROGER SCOTT",
+    post: "MARKETING MANAGER",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    cover: "./images/testo/t3.webp",
+  },
+];
